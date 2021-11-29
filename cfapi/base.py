54402e5a9e54d7b3f49b218033c0fbe14776e4a3
Alon (Ubuntu) <downtown2u@gmail.com>
2021-11-29 22:17:02 +0200

    🎨 Added abstract URL property to `Base`

--- cfapi/base.py
+++ cfapi/base.py
@@ -1,2 +1,10 @@
-class Base:
-    pass
+from abc import ABC, abstractproperty
+
+
+class Base(ABC):
+
+    BASE_URL = "https://codeforces.com/"
+
+    @abstractproperty
+    def url(self):
+        """The URL to the page where the information is scraped from."""
