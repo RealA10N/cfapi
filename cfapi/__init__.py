d53cdbf4094ec8c4ab24ab8092e5f5b346aa8f99
Alon (Ubuntu) <downtown2u@gmail.com>
2021-11-29 21:55:20 +0200

    🎨 Created basic file structure

---
+++ cfapi/__init__.py
@@ -0,0 +1,3 @@
+from .contest import Contest  # noqa: F401
+from .profile import Profile  # noqa: F401
+from .problem import Problem  # noqa: F401
---
+++ cfapi/profile.py
@@ -0,0 +1,5 @@
+from .base import Base
+
+
+class Profile(Base):
+    pass
