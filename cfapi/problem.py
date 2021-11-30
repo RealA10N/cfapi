36858ee6cc0c90d983800f6345ed16271d430ec1
Alon (Ubuntu) <downtown2u@gmail.com>
2021-11-30 16:22:01 +0200

    ✨ Added getters for `Problem` metadata

--- cfapi/problem.py
+++ cfapi/problem.py
@@ -30,3 +30,30 @@ class Problem(Base):
         set, id = match.group("setid", "problemid")
         # TODO: add support for not only contests, but also problemsets, groups, etc.
         return urljoin(self.BASE_URL, f"contest/{set}/problem/{id}")
+
+    def _get_header_entry(self, name: str) -> str:
+        header = self.data.find("div", {"class": "header"})
+        entry = header.find("div", {"class": name})
+        return entry.find(text=True, recursive=False).get_text()
+
+    # ------------------------------------------------------------ Metadata -- #
+
+    @property
+    def title(self) -> str:
+        return self._get_header_entry("title")
+
+    @property
+    def time_limit(self) -> str:
+        return self._get_header_entry("time-limit")
+
+    @property
+    def memory_limit(self) -> str:
+        return self._get_header_entry("memory-limit")
+
+    @property
+    def input_file(self) -> str:
+        return self._get_header_entry("input-file")
+
+    @property
+    def output_file(self) -> str:
+        return self._get_header_entry("output-file")
