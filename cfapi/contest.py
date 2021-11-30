66bcbb22ab20a61dd767b62e8b2cb697ce68f406
Alon (Ubuntu) <downtown2u@gmail.com>
2021-11-30 12:50:59 +0200

    ✨ Added `problems` method to `Contest`

--- cfapi/contest.py
+++ cfapi/contest.py
@@ -1,12 +1,15 @@
 import re
 from urllib.parse import urljoin
+
 from .base import Base
+from .problem import Problem
 
 
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from typing import Optional
+    from typing import Optional, Tuple
+    from bs4 import BeautifulSoup
 
 
 class Contest(Base):
@@ -24,3 +27,18 @@ class Contest(Base):
     def url(self) -> str:
         """The URL to the page where the information is scraped from."""
         return urljoin(self.BASE_URL, f"contest/{self.identifier}")
+
+    @property
+    def _problems_table(self) -> "BeautifulSoup":
+        return self.data.find("table", {"class": "problems"})
+
+    def problems(self) -> "Tuple[Problem]":
+        """Returns a tuple containing lazy-loaded instances that represent
+        the problems in the contest."""
+
+        # Extract all data rows from the problems table,
+        # and ignore the first row which contains the headers.
+
+        rows = self._problems_table.find_all("tr")[1:]
+        links = [row.find("a", href=True)["href"] for row in rows]
+        return tuple(Problem.from_url(link) for link in links)
